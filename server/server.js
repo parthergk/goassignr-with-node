d08813bd88d7bb116b129f2cecad74192d7cb992
gaurav5xy <gauravkumar81464@gmail.com>
2024-05-10 21:07:00 +0530

    last

--- server/server.js
+++ server/server.js
@@ -4,28 +4,22 @@ const Router = require("./routes/data");
 const app = express();
 
 // CORS middleware configuration
-app.use(cors({
-  origin: 'https://goassignr.vercel.app',
-}));
+// app.use(cors({
+//   origin: 'https://goassignr.vercel.app',
+// }));
+
+app.options('*', cors());
 
 // Body parsing middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Logging middleware for requests
-app.use((req, res, next) => {
-  console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
-  next();
-});
+
 
 // Routes
 app.use("/data", Router);
+app.options('/data', cors());
 
-// Error handling middleware
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Something broke!');
-});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
